c305cc0ba3d1d5c3c17bbd092a0b70248e6516e6
Raahil Khan <khanraahil0077@gmail.com>
2025-11-19 02:06:27 +0530

    added new details on security,

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import morgan from 'morgan';
 import employeeRoutes from './api/v1/routes/employee.routes';
 import branchRoutes from './api/v1/routes/branch.routes';
 
+require('dotenv').config();
 const app: Application = express();
-const port = 3000; // Define port (use 3001 if 3000 is in use)
+const port = process.env.PORT || 3000; // Define port (use 3001 if 3000 is in use)
 
 app.use(morgan('combined')); // Log HTTP requests
 app.use(express.json()); // Parse JSON request bodies
